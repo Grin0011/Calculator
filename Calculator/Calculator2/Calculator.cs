c0c471b41b88c1e60fe16509b9d45192260c4e9b
Joan Grinwis <grin0011@hz.nl>
2017-11-01 14:18:37 +0100

    Initial

---
+++ Calculator/Calculator2/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Calculator2
+{
+    public class Calculator : CalculatorMusthaves
+    {
+        public Calculator()
+        {
+            
+        }
+
+        int CalculatorMusthaves.Diff(int getal1, int getal2)
+        {
+            return getal1 - getal2;
+            throw new NotImplementedException();
+        }
+
+        int CalculatorMusthaves.Product(int getal1, int getal2)
+        {
+            return getal1 * getal2;
+            throw new NotImplementedException();
+        }
+
+        int CalculatorMusthaves.Quotient(int getal1, int getal2)
+        {
+            return getal1 % getal2;
+            throw new NotImplementedException();
+        }
+
+        int CalculatorMusthaves.Sum(int getal1, int getal2)
+        {
+            return getal1 + getal2;
+            throw new NotImplementedException();
+        }
+    }
+}
